4f94f951221dfd3804cf1eaf01ddec2b05be8e14
chrischep <christine.chepleting@student.moringaschool.com>
2022-10-27 08:47:52 +0300

    code along

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,8 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+#inherits fro vehicle class
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
